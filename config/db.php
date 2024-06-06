e3ee7e241b931e3003dba5e900076822d35370e5
Fran Velásquez <141286555+AusVel21@users.noreply.github.com>
2024-06-05 20:24:26 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
     class db{
-        private $host= "localhost:3306";
-        private $dbname="prueba";
-        private $user="root";
-        private $password="";
+        private $host= "betbhf5auxtujwdfwbgo-mysql.services.clever-cloud.com";
+        private $dbname="betbhf5auxtujwdfwbgo";
+        private $user="uwgrzmcslucwwsfd";
+        private $password="9tj3Zpq3s0uH3o3YqDjS";
         public function conexion(){
             try{
                 $PDO = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
