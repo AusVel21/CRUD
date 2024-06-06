5b25a221b97017f418d5b7a74c7ac9abef20b4e7
Fran Velásquez <141286555+AusVel21@users.noreply.github.com>
2024-06-05 21:41:53 -0600

    Update get_record_details.php

--- view/head/get_record_details.php
+++ view/head/get_record_details.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '../../config/db.php';
-require_once __DIR__ . '../../model/usernameModel.php';
+require_once __DIR__ . '/../../config/db.php';
+require_once __DIR__ . '/../../model/usernameModel.php';
 
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
